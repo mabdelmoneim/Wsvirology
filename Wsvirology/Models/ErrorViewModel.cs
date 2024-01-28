71c652f9e097a5bed8c1837df88eab9a0f30836b
Mohamed Ahmed <mabdelmoneim678@gmail.com>
2024-01-28 18:54:34 +0200

    Add project files.

---
+++ Wsvirology/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wsvirology.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
